4ba719d2ffe6b1a41f576fec149751c6a2362a49
Gabriel Miranda <gabrielmiranda282@hotmail.com>
2024-10-08 23:58:50 +0000

    feat: criando repositorio em memoria

---
+++ src/shared/db/in-memory/in-memory.repository.ts
@@ -0,0 +1,35 @@
+import { Entity } from "../../domain/entity";
+import { IRepository } from "../../domain/repositories/repository.interface";
+import { ValueObject } from "../../domain/value-object";
+
+export abstract class InMemoryRepository<E extends Entity, EntityId extends ValueObject> implements IRepository<E,EntityId>{
+  items: E[] = []
+  async insert(entity: any): Promise<void> {
+    this.items.push(entity)
+  }
+ async bulkInsert(entities: any[]): Promise<void> {
+    this.items.push(...entities)
+  }
+  async update(entity: E): Promise<void> {
+    const index = this.items.findIndex((item) => item.entityId.equals(entity.entityId))
+    if (index === -1) throw new Error("Entity not found")
+    this.items[index] = entity
+  }
+  async delete(entity: E): Promise<void> {
+    const index = this.items.findIndex((item) => item.entityId.equals(entity.entityId))
+    if (index === -1) throw new Error("Entity not found")
+    this.items.splice(index, 1)
+    }
+
+  async findById(id: EntityId): Promise<E|null> {
+    const item = this.items.find((item) => item.entityId.equals(id))
+    return item||null
+  }
+
+  
+  async findAll(): Promise<E[]> {
+    return this.items
+  }
+  abstract getEntity(): new (...args: any[]) => E 
+
+}
