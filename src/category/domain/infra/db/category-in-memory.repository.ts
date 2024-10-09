97cc80768a85d1899401d47d22b9af673986fa12
Gabriel Miranda <gabrielmiranda282@hotmail.com>
2024-10-09 14:25:41 +0000

    feat: Adicionar suporte a pesquisa no repositório em memória
    Adiciona suporte para pesquisa no repositório em memória, permitindo filtrar e ordenar os itens da categoria.

--- src/category/domain/infra/db/category-in-memory.repository.ts
+++ src/category/domain/infra/db/category-in-memory.repository.ts
@@ -1,9 +1,35 @@
+import { SortDirection } from "../../../../shared/domain/repositories/search-params";
 import { Uuid } from "../../../../shared/domain/value-objects/uuid.vo";
-import { InMemoryRepository } from "../../../../shared/infra/db/in-memory/in-memory.repository";
+import { InMemorySearchableRepository } from "../../../../shared/infra/db/in-memory/in-memory.repository";
 import { Category } from "../../category.entity";
 
-export class CategoryInMemoryRepository extends InMemoryRepository<Category,Uuid> {
+
+export class CategoryInMemoryRepository extends InMemorySearchableRepository<
+  Category,
+  Uuid
+> {
+  sortableFields: string[] = ["name", "createdAt"];
+  protected async applyFilter(
+    items: Category[],
+    filter: string
+  ): Promise<Category[]> {
+    if (!filter) {
+      return items;
+    }
+    return items.filter((i) => {
+      return i.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  }
   getEntity(): new (...args: any[]) => Category {
-    return Category
+    return Category;
+  }
+  protected applySort(
+    items: Category[],
+    sort: string | null,
+    sort_dir: SortDirection | null
+  ) {
+    return sort
+      ? super.applySort(items, sort, sort_dir)
+      : super.applySort(items, "createdAt", "desc");
   }
 }
