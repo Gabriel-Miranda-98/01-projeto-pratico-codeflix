4890b6775bac36aa0b18e73b58192546b1f554c0
Gabriel Miranda <gabrielmiranda282@hotmail.com>
2024-10-14 20:57:39 +0000

    feat: criando caso de uso e teste para editar uma categoria

---
+++ src/category/application/update-category.use-case.ts
@@ -0,0 +1,60 @@
+import { IUseCase } from "../../shared/application/use-case.interface";
+import { NotFoundError } from "../../shared/domain/errors/not-found.error";
+import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+import { Category } from "../domain/category.entity";
+import { ICategoryRepository } from "../domain/repositories/category.repository";
+
+type UpdateCategoryInput = {
+  id: string
+  name?: string
+  description?: string|null
+  isActive?: boolean
+}
+
+
+type UpdateCategoryOutput = {
+  id: string
+  name: string
+  description: string|null
+  isActive: boolean,
+  createdAt: Date
+}
+
+export class UpdateCategoryUseCase implements IUseCase<UpdateCategoryInput, UpdateCategoryOutput> {
+  constructor(private readonly categoryRepository: ICategoryRepository) {}
+  async execute(input: UpdateCategoryInput): Promise<UpdateCategoryOutput> {
+    const categoryId = Uuid.create(input.id)
+    const category = await this.categoryRepository.findById(categoryId)
+
+    if (!category) {
+      throw new NotFoundError(categoryId, Category)
+    }
+
+    if (input.name) {
+      category.changeName(input.name)
+    }
+
+    if ("description" in input) {
+      category.changeDescription(input.description);
+    }
+
+    if (input.isActive===true) {
+      category.activate()
+    }
+
+    if (input.isActive===false) {
+      category.deactivate()
+    }
+    await this.categoryRepository.update(category)
+
+    return {
+      id: category.categoryId.id,
+      name: category.name,
+      description: category.description,
+      isActive: category.isActive,
+      createdAt: category.createdAt
+      
+    }
+
+  }
+}
