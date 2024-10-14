c7ba212cdef9de2a1431ccfeb3afce57d105008b
Gabriel Miranda <gabrielmiranda282@hotmail.com>
2024-10-14 22:11:44 +0000

    feat: criando mapper para output para categoria

---
+++ src/category/application/use-cases/common/category-output.test.ts
@@ -0,0 +1,37 @@
+import { CategoryOutputMapper } from './category-output';
+import { Category } from '../../../domain/category.entity';
+
+describe('CategoryOutputMapper', () => {
+  it('should map Category to CategoryOutput correctly', () => {
+    const mockCategory = Category.fake().aCategory().build();
+    const spyToJSON = jest.spyOn(mockCategory, 'toJSON');
+    const expectedOutput = {
+      id: mockCategory.categoryId.id,
+      name: mockCategory.name,
+      description: mockCategory.description,
+      isActive: mockCategory.isActive,
+      createdAt: mockCategory.createdAt,
+    };
+
+    const output = CategoryOutputMapper.toOutput(mockCategory);
+    expect(output).toEqual(expectedOutput);
+    expect(spyToJSON).toHaveBeenCalledTimes(1);
+  });
+
+  it('should handle null description correctly', () => {
+    const mockCategory = Category.fake().aCategory().withDescription(null).build();
+    const spyToJSON = jest.spyOn(mockCategory, 'toJSON');
+
+    const expectedOutput = {
+      id: mockCategory.categoryId.id,
+      name: mockCategory.name,
+      description: mockCategory.description,
+      isActive: mockCategory.isActive,
+      createdAt: mockCategory.createdAt,
+    };
+
+    const output = CategoryOutputMapper.toOutput(mockCategory);
+    expect(output).toEqual(expectedOutput);
+    expect(spyToJSON).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/category/application/use-cases/common/category-output.ts
@@ -0,0 +1,21 @@
+import { Category } from "../../../domain/category.entity";
+
+export type CategoryOutput = {
+  id: string;
+  name: string;
+  description: string|null;
+  isActive: boolean;
+  createdAt: Date;
+  
+}
+
+
+export class CategoryOutputMapper {
+  static toOutput(category: Category): CategoryOutput {
+    const {categoryId,...otherProps} = category.toJSON();
+    return {
+      id: category.categoryId.id,
+      ...otherProps,
+    };
+  }
+}
