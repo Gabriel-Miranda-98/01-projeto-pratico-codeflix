994a2f62f2ebbe693ec30b0a227228143307aed4
Gabriel Miranda <gabrielmiranda282@hotmail.com>
2024-10-16 18:37:43 +0000

    feat: criando module de categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({}),
+    CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
