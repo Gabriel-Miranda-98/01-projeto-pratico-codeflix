82059c977eabeebeee0c6711d89b55d05735ba40
Gabriel Miranda <gabrielmiranda282@hotmail.com>
2024-10-16 18:51:32 +0000

    chore: configurando sequelize e respositorio de categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({}),
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: ':memory:',
+      logging: false,
+      models:[CategoryModel],
+    }),
     CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
